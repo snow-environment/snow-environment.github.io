4631ec539de47dbefbad88cc8e0610b953e1435a
Georgi Balabanov <88368087+git-Ball@users.noreply.github.com>
2022-10-17 17:45:46 +0300

    Validate logic.

--- src/servicenowConn.js
+++ src/servicenowConn.js
@@ -1,21 +1,11 @@
-//  import sn from './lib.js';
-// import * as sn from './lib/servicenow.js'
 
-// import { ServiceNow } from "./lib/servicenowAsync.js";
-
-// import { ServiceNow } from "./lib/servicenow copy.js";
 import { ServiceNow } from "./lib/servicenowAsync.js";
 
-// -----------------------
-
-
-
-// -----------------------
 
 
 
 
-// import * as api from './lib/servicenow copy.js'
+
 
 
 export async function snowConnection(par,par2,par3) {
@@ -37,71 +27,6 @@ if(auth.status !== 200){
   return false;
 }
 
-    // let auth =sN.Authenticate();
-    // let response = await auth.response.json()
-    // console.log('------------->',auth)
-    // const connection = ServiceNow.Authenticate();
-    // console.log(sn)
-  }
-// snowConnection();
-
-// const sn = require("servicenow-rest-api"); // Call the API
-// // const ServiceNow = new sn(); // Create SN client with credentials
-// const ServiceNow = new sn("dev109438", "admin", "LrmsjVJB@8^3"); // Create SN client with credentials
-// module.exports = { ServiceNow }; // Exports the "ServiceNow" variable for later use 
-// ServiceNow.Authenticate(); // Authenticate with SNOW instance
 
 
-// import { ServiceNow } from "./lib/servicenowAsync.js";
-
-
-
-// export async function snowConnection(par, par2, par3) {
-
-//   let sn = ServiceNow;
-
-//   // const sN = new sn("dev109438", "admin", "LrmsjVJB@8^3");
-
-//   const connector = new sn(par, par2, par3, true);
-
-//   connector.getSampleData("sys_user", (res) => {
-
-//     console.log(res)
-
-//     if (res.length > 0) {
-
-//       let auth = connector.Authenticate();
-
-//       console.log(auth.status)
-
-//       console.log("Connected");
-
-//       return true;
-
-//     } else {
-
-//       console.log("Failed to connect");
-
-//       return false;
-
-//     }
-
-//   });
-
-  // let auth = await sN.Authenticate();
-
-  // console.log('auth file', auth)
-
-  // if (auth.status == 200) {
-
-  //   return true;
-
-  // }
-
-  // if (auth.status !== 200) {
-
-  //   return false;
-
-  // }
-
-// }
+}
--- src/testOne.js
+++ src/testOne.js
@@ -1,40 +1,63 @@
-function verifyTask() {
-  const { ServiceNow } = require("./servicenowConn.js"); // Import "ServiceNow" variable from servicenowConn.js
+import { credentials } from "./app.js";
+import { ServiceNow } from "./lib/servicenowAsync.js";
+
+
+export async function verifyTask() {
+
+  let sn = ServiceNow;
+
+
+
+  const sN = new sn(credentials.instName, credentials.instUserName, credentials.instPassword, true);
+  sN.Authenticate();
+  console.log(sN)
+
   let isTableFound = false;
   let isDescriptionFound = false;
   let isShortDescriptionFound = false;
   let isAssignedToFound = false;
 
   // Get data from table
-  ServiceNow.getSampleData("u_project_verify", (res) => {
-    if (res.length > 0) {
-      console.log(`Test One: Table "u_project_verify" found.`);
-      isTableFound = true;
-      for (let i = 0; i < res.length; i++) {
-        const object = res[i];
-        for (const key in object) {
-          if (key == "u_description" && object[key] == "Test Task 1") {
-            isDescriptionFound = true;
-            console.log(`Test One: Field "${key}" found with value "${object[key]}".`);
-          }
-          if (key == "u_short_description" && object[key] == "Test 1") {
-            isShortDescriptionFound = true;
-            console.log(`Test One: Field "${key}" found with value "${object[key]}".`);
-          }
-          if (key == "u_assigned_to" && object[key] == "5695d69673500010c2e7660c4cf6a789"){
-            isAssignedToFound = true;
-            console.log(`Test One: Field "${key}" found with value "${object[key]}".`);
-          }
-        }
-      }
-    }
-    if (isTableFound && isDescriptionFound && isShortDescriptionFound && isAssignedToFound){
-      console.log("Test One Completed")
-      return true;
-    } else {
-      console.log("Test One Failed")
-      return false;
-    }
+  console.log('----')
+  sN.getSampleData('sys_user',(res)=>{
+   for (let index = 0; index < res.length; index++) {
+     console.log(res[index])
+    
+   }
+
+    console.log('bachka')
+    // if (res.length > 0) {
+    //   console.log(`Test One: Table "u_project_verify" found.`);
+    //   isTableFound = true;
+    //   for (let i = 0; i < res.length; i++) {
+    //     const object = res[i];
+    //     for (const key in object) {
+    //       if (key == "u_description" && object[key] == "Test Task 1") {
+    //         isDescriptionFound = true;
+    //         console.log(`Test One: Field "${key}" found with value "${object[key]}".`);
+    //       }
+    //       if (key == "u_short_description" && object[key] == "Test 1") {
+    //         isShortDescriptionFound = true;
+    //         console.log(`Test One: Field "${key}" found with value "${object[key]}".`);
+    //       }
+    //       if (key == "u_assigned_to" && object[key] == "5695d69673500010c2e7660c4cf6a789"){
+    //         isAssignedToFound = true;
+    //         console.log(`Test One: Field "${key}" found with value "${object[key]}".`);
+    //       }
+    //     }
+    //   }
+    // }
+    // if (isTableFound && isDescriptionFound && isShortDescriptionFound && isAssignedToFound){
+    //   console.log("Test One Completed")
+    //   return true;
+    // } else {
+    //   console.log("Test One Failed")
+    //   return false;
+    // }
   });
+
 }
-verifyTask();
+// verifyTask();
+
+
+
