dee5f0cc9bf09e27186adae17f500784365085a7
Georgi Balabanov <88368087+git-Ball@users.noreply.github.com>
2022-10-19 11:17:46 +0300

    Fix some task issues.

--- src/projects.js
+++ src/projects.js
@@ -2,40 +2,10 @@ import {render, html} from './lib.js'
 const tasksTemplate = () => html`
 <section class="taskOne">
   <h1 class="project-heading">Mini Project #1</h1>
-  <div class="task-wrapper">
-      <h2>Task 1</h2>
-      <div class="short-description">
-        <p class="task-info">1. Create a custom table with label 'Project Verify' and name 'u_project_verify':</p>
-   <ul class="requirements">
-       <li class="req-for-task">create a custom field with column label 'Description' and column name 'u_description'. Field type 'String' with max lenght 100</li>
-       <li class="req-for-task">create a custom field with column label 'Short Description' and column name 'u_short_description'. Field type 'String' with max lenght 20</li>
-       <li class="req-for-task">create a custom field with column label 'Assigned to' and column name 'u_assigned_to'. Field type 'List'. Set the reference of the field to 'sys_user' table</li>
-       <li class="req-for-task">(Optional) modify the default field layout(or create a new layout) to display only the following fields --> 'Description', 'Short Description', 'Assigned to','Created by' and 'Created'(last two fields are existing by default)</li>
-   </ul>
-      </div>
-      <!--  -->
-      <div class="short-description">
-          <p class="task-info" >2. In the newly created table, create a new record:</p>
-     <ul class="requirements">
-         <li class="req-for-task">'Description' = 'Test Task 1'</li>
-         <li class="req-for-task">'Short Description' = 'Test 1'</li>
-         <li class="req-for-task">'Created by' = 'admin' (by default)</li>
-         <li class="req-for-task">'Assigned to' = 'Jason Roy'(choose user from the choice menu)</li>
-         <li class="req-for-task">'Created' = Current date and time</li>
-     </ul>
-        </div>
-        <div class="button-wrapper">
-        <button  class="validate-task-btn">
-            Validate
-          </button>
-          <p class="not-validated">Please make sure you complete all requirements!</p>
-          <p class="validated">Task is completed!</p>
-
-    </div>
-  </div>
+ 
   
   <div class="task-wrapper">
-      <h2>Task 2</h2>
+      <h2>Task 1</h2>
       <div class="short-description">
         <p class="task-info">1. Create a new user:</p>
    <ul class="requirements">
@@ -63,10 +33,40 @@ const tasksTemplate = () => html`
             Validate
           </button>
           <p class="not-validated">Requirements are not complete!</p>
-          <p class="validated">Task is completed!</p>
-
+          
     </div>
   </div>
+  <div class="task-wrapper">
+    <h2>Task 2</h2>
+    <div class="short-description">
+      <p class="task-info">1. Create a custom table with label 'Project Verify' and name 'u_project_verify':</p>
+ <ul class="requirements">
+     <li class="req-for-task">create a custom field with column label 'Description' and column name 'u_description'. Field type 'String' with max lenght 100</li>
+     <li class="req-for-task">create a custom field with column label 'Short Description' and column name 'u_short_description'. Field type 'String' with max lenght 20</li>
+     <li class="req-for-task">create a custom field with column label 'Assigned to' and column name 'u_assigned_to'. Field type 'List'. Set the reference of the field to 'sys_user' table</li>
+     <li class="req-for-task">(Optional) modify the default field layout(or create a new layout) to display only the following fields --> 'Description', 'Short Description', 'Assigned to','Created by' and 'Created'(last two fields are existing by default)</li>
+ </ul>
+    </div>
+    <!--  -->
+    <div class="short-description">
+        <p class="task-info" >2. In the newly created table, create a new record:</p>
+   <ul class="requirements">
+       <li class="req-for-task">'Description' = 'Test Task 1'</li>
+       <li class="req-for-task">'Short Description' = 'Test 1'</li>
+       <li class="req-for-task">'Created by' = 'admin' (by default)</li>
+       <li class="req-for-task">'Assigned to' = 'Jason Roy'(choose user from the choice menu)</li>
+       <li class="req-for-task">'Created' = Current date and time</li>
+   </ul>
+      </div>
+      <div class="button-wrapper">
+      <button  class="validate-task-btn">
+          Validate
+        </button>
+        <p class="not-validated">Please make sure you complete all requirements!</p>
+        <p class="validated">Task is completed!</p>
+
+  </div>
+</div>
 
   <div class="task-wrapper">
       <h2>Task 3</h2>
