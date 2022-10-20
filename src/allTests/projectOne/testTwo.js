2f5f199c1cfc5adb4cbfe13073ee30340e7947cb
Aleksandar Karastoyanov <karastoqnov.alexandar@gmail.com>
2022-10-20 11:27:56 +0300

    Project One Test Two Updated
     - Joined Query for `sys_user_grmember` is implemented for Second Test;
    
     - Third Test of the Project One remaining under development

--- src/allTests/projectOne/testTwo.js
+++ src/allTests/projectOne/testTwo.js
@@ -7,7 +7,7 @@ export async function oneTwo() {
     connector.Authenticate();
     console.log(connector);
 
-   
+
     // Check if the Group is Created
     let isGroupFound = false;
     const fieldsFindGroup = [
@@ -19,11 +19,9 @@ export async function oneTwo() {
     ];
     const filtersFindGroup = [
         'name=ServiceNow MiniProject One'
-    ]
+    ];
 
     const findGroup = await connector.getTableData(fieldsFindGroup, filtersFindGroup, 'sys_user_group', function (res) { console.log(res) });
-    // console.log(findGroup);
-    // console.log(findGroup.data.result);
     if (findGroup.data.result.length > 0) {
         for (const key in findGroup) {
             if (key == "data") {
@@ -35,6 +33,7 @@ export async function oneTwo() {
                             for (const k in managerName) {
                                 if (managerName["display_value"] == "Abel Tuter") {
                                     isGroupFound = true;
+                                    console.log("Group ServiceNow MiniProject One is found.")
                                 }
                             }
                         }
@@ -44,8 +43,38 @@ export async function oneTwo() {
         }
     }
 
-    // Check if User is Assigned to the Group
+
+
+    // Check if User is Assigned to the Group sys_user.sys_user_grmember.user_list
     let isGroupAssigned = false;
+    const fieldsFindGroupAssigned = [
+        "group",
+        "user",
+    ];
+    const filtersFindGroupAssigned = [
+        // Leave empty
+    ];
+    const findGroupAssigned = await connector.getTableData(fieldsFindGroupAssigned, filtersFindGroupAssigned, 'sys_user_grmember', function (res) { console.log(res) });
+    // console.log(findGroupAssigned.data.result);
+    let firstGroupFound = false;
+    let secondGroupFound = false;
+    for (let i = 0; i < findGroupAssigned.data.result.length; i++) {
+        let currentUser = findGroupAssigned.data.result[i]['user']['display_value'];
+        let currentGroup = findGroupAssigned.data.result[i]['group']['display_value'];
+        if (currentUser == "John Doe" && currentGroup == "Service Desk") {
+            firstGroupFound = true;
+        }
+        if (currentUser == "John Doe" && currentGroup == "ServiceNow MiniProject One") {
+            secondGroupFound = true;
+        }
+        if (firstGroupFound == true && secondGroupFound == true) {
+            isGroupAssigned = true;
+            console.log("User John Doe is in Service Desk and Service Now MiniProject One.")
+            break;
+        }
+    }
+
+
 
 
     // Check if Roles are Assigned to the User
