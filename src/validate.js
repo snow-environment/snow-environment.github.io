f7b7ef67ab43ee7965c6b4eabf2a2f83c7b24839
Georgi Balabanov <88368087+git-Ball@users.noreply.github.com>
2022-10-21 12:50:09 +0300

    Fix validation with buttons

--- src/validate.js
+++ src/validate.js
@@ -1,17 +1,20 @@
-export function validation(response,btn,notValidated){
+export function validation(response, btn, notValidated) {
 
-    if (response) {
-      btn.style.display = 'none'
-      document.querySelector('.validated').style.display = 'block'
-    }
-    else {
-      btn.disabled = false;
-      btn.classList.remove('no-hover')
-      btn.classList.remove('grey-me')
-  
-      notValidated.style.display = 'block'
-  
-  
-    }
-    // return btn,notValidated
+  if (response) {
+    btn.style.display = 'none'
+
+    btn.parentNode.querySelector('.validated').style.display = 'block'
+
+
   }
+  else {
+    btn.disabled = false;
+    btn.classList.remove('no-hover')
+    btn.classList.remove('grey-me')
+
+    notValidated.style.display = 'block'
+
+
+  }
+
+}
