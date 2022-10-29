05f7f644251ccd2c2e01743f78ad7e882385ce7b
Aleksandar Karastoyanov <karastoqnov.alexandar@gmail.com>
2022-10-29 23:45:49 +0300

    Project Two Task One Update
    - queries implemented
    
    - function returns True/False
    
    -implemented in frontend

--- src/allTests/projectTwo/testOne.js
+++ src/allTests/projectTwo/testOne.js
@@ -9,23 +9,46 @@ export async function twoOne() {
 
     // Check if user exists
     let isUserFound = false; 
-    const fields =[
+    const fieldsUserFound =[
         "user_name"
     ];
 
-    const filters = [
+    const filtersUserFound = [
         "user_name=snow_mid_user"
     ];
-    const tableData = await connector.getTableData(fields, filters, 'sys_user', function (res) { console.log(res) });
+    const tableData = await connector.getTableData(fieldsUserFound, filtersUserFound, 'sys_user', function (res) { console.log(res) });
     if (tableData.data.result.length > 0) {
         console.log("Mid user found.")
-        return true;
+        isUserFound = true;
     }
 
 
     // Check if user has the role
     let isRoleAssigned = false;
+    const fieldsRoleAssigned = [
+        'user',
+        'role',
+    ];
+    const filtersRoleAssigned = [
+        // Leave empty, XML file for the table sys_user_has_role in ServiceNow returns only SysIDs and not the values of the records
 
+    ];
+    const roleAssigned = await connector.getTableData(fieldsRoleAssigned, filtersRoleAssigned, 'sys_user_has_role', function (res) {console.log(res)}); 
+    for (let i = 0; i < roleAssigned.data.result.length; i++) {
+        let currentUser = roleAssigned.data.result[i]['user']['display_value'];
+        let currentRole = roleAssigned.data.result[i]['role']['display_value'];
+        if (currentRole == "mid_server" && currentUser == "Raymond Denton") {
+            isRoleAssigned = true;
+            break;
+        }
+    }
+
+
+    if (isUserFound && isRoleAssigned) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
