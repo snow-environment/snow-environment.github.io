b2e4a3cda3c73ab15494f8e75e8e9ebcdcd17b1d
Aleksandar Karastoyanov <karastoqnov.alexandar@gmail.com>
2022-10-30 15:42:03 +0200

    Project Two Test Three Updated

--- src/allTests/projectTwo/testThree.js
+++ src/allTests/projectTwo/testThree.js
@@ -1,7 +1,30 @@
-
-
-
+import { credentials } from "../../app.js";
+import { ServiceNow } from "../../lib/servicenowAsync.js";
 
 export async function twoThree() {
-    // TO DO
+    const sn = ServiceNow;
+    const connector = new sn(credentials.instName, credentials.instUserName, credentials.instPassword, true);
+    connector.Authenticate();
+    console.log(connector);
+
+    // Get credentials
+    let isCredentialsFound = false;
+    const fieldsCredsFound= [
+        'name',
+        'classification',
+        'user_name'
+    ];
+    const filtersCredsFound= [
+        'name=SNOW_Linux_Credentials',
+        'user_name=vagrant',
+        'classification=ssh'
+    ];
+    const tableDataCredentialsFound = await connector.getTableData(fieldsCredsFound, filtersCredsFound, 'discovery_credentials', function (res) {console.log(res)});
+    if (tableDataCredentialsFound.data.result.length > 0) {
+        isCredentialsFound = true;
+    }
+
+    // Check for Linux Serve CI
+    let isLinuxVMFound = false;
+   
 }
