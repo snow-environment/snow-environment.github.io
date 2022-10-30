9a060325dfda4e356b0d857764b1f323cecf2cb2
Aleksandar Karastoyanov <karastoqnov.alexandar@gmail.com>
2022-10-30 08:54:48 +0200

    Project Two Task Two Update

--- src/allTests/projectTwo/testTwo.js
+++ src/allTests/projectTwo/testTwo.js
@@ -1,6 +1,31 @@
-
-
+import { credentials } from "../../app.js";
+import { ServiceNow } from "../../lib/servicenowAsync.js";
 
 export async function twoTwo() {
-    // TO DO
+    const sn = ServiceNow;
+    const connector = new sn(credentials.instName, credentials.instUserName, credentials.instPassword, true);
+    connector.Authenticate();
+    console.log(connector);
+
+    let isMidFound = false;
+    const fieldsMidFound = [
+        'name',
+        'status',
+        'validated',
+        'user_name'
+    ];
+    const filtersMidFound = [
+        'name=SNOW_MID',
+        'status=Up',
+        'validated=true',
+        'user_name=snow_mid_user'
+    ];
+    const tableData = await connector.getTableData(fieldsMidFound, filtersMidFound, 'ecc_agent', function (res) {console.log(res)});
+    if (tableData.data.result.length > 0) {
+        console.log("MID Server is Found. Status is UP and the server is Validated.")
+        return true;
+    } else {
+        return false;
+    }
+
 }
