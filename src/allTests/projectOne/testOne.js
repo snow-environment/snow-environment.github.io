4d783d09b6d72fa98adfcb2464340a7709804e04
Aleksandar Karastoyanov <karastoqnov.alexandar@gmail.com>
2022-11-01 22:41:22 +0200

    Project One Test One code improve
     - code length optimized
    
     - faster execution time

--- src/allTests/projectOne/testOne.js
+++ src/allTests/projectOne/testOne.js
@@ -7,29 +7,16 @@ export async function oneOne() {
   connector.Authenticate();
   console.log(connector)
 
+  // Get table 'sys_user'
   let isTableFound = false;
-  let isFirstNameFound = false;
-  let isLastNameFound = false;
-  let isEmailAddressFound = false;
-
-  // Get table 
   const tableData = await connector.getSampleData('sys_user', (res) => { console.log(res) });
-  if (tableData.status == 200) {
-    let resultTable = '';
-    for (const key in tableData) {
-      if (key == "data") {
-        resultTable = tableData[key].result;
-        if (resultTable.length > 0) {
-          isTableFound = true;
-          console.log("Test One: Table Found.")
-        }
-      }
-    }
+  if (tableData.data.result.length > 0) {
+    isTableFound = true;
   }
-  console.log(tableData);
 
 
-  // Get data from table
+  // Get data/specific record John Doe from table
+  let isUserFound = false;
   const fields = [
     'first_name',
     'last_name',
@@ -38,44 +25,18 @@ export async function oneOne() {
   const filters = [
     'first_name=John',
     'last_name=Doe',
+    'email=john.doe@example.com'
   ];
   const tableRecords = await connector.getTableData(fields, filters, 'sys_user', function (res) { console.log(res) });
-  // console.log(tableRecords);
-  // console.log(tableRecords.data.result);
   if (tableRecords.data.result.length > 0) {
-    let resultQuery = '';
-    for (const key in tableRecords) {
-      if (key == "data") {
-        let resultQuery = tableRecords[key].result[0];
-        console.log(resultQuery);
-        if (resultQuery["first_name"] == "John") {
-          isFirstNameFound = true;
-        }
-        if (resultQuery["last_name"] == "Doe") {
-          isLastNameFound = true;
-        }
-        if (resultQuery["email"] == "john.doe@example.com") {
-          isEmailAddressFound = true;
-        }
-      }
-    }
+    isUserFound = true;
   }
 
-  if (isTableFound && isFirstNameFound && isLastNameFound && isEmailAddressFound) {
-    console.log("Test One:  All Tasks Completed.");
+
+
+  if (isTableFound && isUserFound) {
     return true;
   } else {
-    console.log("Test One: Tasks Not Completed.");
-    // Use the logic below to display which specific task was not completed
-    if (!isTableFound) {
-      console.log("Table `sys_user` not found."); // Basically imposible due to the fact that sys_user is a base table
-    }
-    if (!isFirstNameFound || !isLastNameFound) {
-      console.log("User with name `John Doe` not created.")
-    }
-    if (!isEmailAddressFound) {
-      console.log("Email address for user `John Doe` is not correct.")
-    }
     return false;
   }
 }
